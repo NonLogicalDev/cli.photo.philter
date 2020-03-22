1fed4a95a38314cc72c7ae4ec7551846f470d9cd
Oleg Utkin <oleg@nonlogical.io>
2020-03-22 16:10:20 -0700

    Tidy up the repo, prepare for release

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="philter",
     version="0.0.1",
     description="LUT creation toolkit.",
     author="nonlogicaldev",
 
-    install_requires=requirements,
+    install_requires=[
+        "numpy>=1.16.2",
+        "Pillow>=6.0.0",
+    ],
+
     packages=find_packages(),
     scripts=[
         "bin/philter"
