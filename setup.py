76d3aff097c4cc56cc7e9a9887610d8dc2d2ed03
Oleg Utkin <oleg.utkin@uber.com>
2020-03-27 13:40:10 -0700

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="philter",
-    version="0.0.1",
-    description="LUT creation toolkit.",
+    name="nl-philter",
+    version="0.0.2",
     author="nonlogicaldev",
+
+    description="cLUT/Cube toolkit for recording color transformations.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     install_requires=[
         "numpy>=1.16.2",
-        "Pillow>=6.0.0",
+        "Pillow>=6.2.0",
     ],
 
     packages=find_packages(),
